3d402b94a8d7926b53f9e9eb2d9d3c4bb9127d61
Sebastian <seba172i@mail.hansenberg.dk>
2023-09-06 12:26:42 +0200

    Refininnenenen

--- Program.cs
+++ Program.cs
@@ -5,25 +5,28 @@ Book mybook = new Book("My Notes");
 List<Chapter> chapters = new List<Chapter> ();
 Chapter chapter1 = new Chapter ("vars");
 Chapter chapter2 = new Chapter ("BDT");
+Chapter chapter3 = new Chapter ("");
 chapters.Add (chapter1);
 chapters.Add (chapter2);
 
-Page side1 = new Page(MyNotes.VariablesPageOne);
-Page side2 = new Page(MyNotes.VariablesPageTwo);
+Page Vside1 = new Page(MyNotes.VariablesPageOne);
+Page Vside2 = new Page(MyNotes.VariablesPageTwo);
+Page Vside3 = new Page(MyNotes.VariablesPageThree);
 
 
 
-chapter1.Pages.Add(side1);
-chapter1.Pages.Add(side2);
+chapter1.Pages.Add(Vside1);
+chapter1.Pages.Add(Vside2);
+chapter1.Pages.Add(Vside3);
 
 
 Chapter kap2 = new Chapter("BDT");
 
-Page side3 = new Page (MyNotes.BasicDataTypesPageOne);
-Page side4 = new Page (MyNotes.BasicDataTypesPageTwo);
+Page Bside1 = new Page (MyNotes.BasicDataTypesPageOne);
+Page Bside2 = new Page (MyNotes.BasicDataTypesPageTwo);
 
-chapter2.Pages.Add(side3);
-chapter2.Pages.Add(side4);
+chapter2.Pages.Add(Bside1);
+chapter2.Pages.Add(Bside2);
 
 
 mybook.Chapters = chapters;
