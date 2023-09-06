cd25cddba73f73d92e161ce46a857ff7084851d5
Sebastian <seba172i@mail.hansenberg.dk>
2023-09-06 11:11:38 +0200

    Added MyBook start

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿Page side1 = new Page(MyNotes.VariablesPageOne);
+﻿Book mybook = new Book("My Notes");
+mybook.Chapters.Add(kap1);
+mybook.Start();
+
+Page side1 = new Page(MyNotes.VariablesPageOne);
 Page side2 = new Page(MyNotes.VariablesPageTwo);
 
 Chapter kap1 = new Chapter("vars");
@@ -6,7 +10,6 @@ Chapter kap1 = new Chapter("vars");
 kap1.Pages.Add(side1);
 kap1.Pages.Add(side2);
 
-kap1.Read();
 
 Chapter kap2 = new Chapter("BDT");
 
@@ -16,4 +19,4 @@ Page side4 = new Page (MyNotes.BasicDataTypesPageTwo);
 kap2.Pages.Add(side3);
 kap2.Pages.Add(side4);
 
-kap2.Read();
+
