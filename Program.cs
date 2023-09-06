c64ffe63d681d6408a8489aa12c590d8cde9d70f
Sebastian <seba172i@mail.hansenberg.dk>
2023-09-06 12:22:01 +0200

    Something

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 
 Book mybook = new Book("My Notes");
 
+List<Chapter> chapters = new List<Chapter> ();
+Chapter chapter1 = new Chapter ("vars");
+Chapter chapter2 = new Chapter ("BDT");
+chapters.Add (chapter1);
+chapters.Add (chapter2);
+
 Page side1 = new Page(MyNotes.VariablesPageOne);
 Page side2 = new Page(MyNotes.VariablesPageTwo);
 
@@ -19,9 +25,6 @@ Page side4 = new Page (MyNotes.BasicDataTypesPageTwo);
 kap2.Pages.Add(side3);
 kap2.Pages.Add(side4);
 
-List<Chapter> chapters = new List<Chapter> ();
-Chapter chapter1 = new Chapter ("vars", pages);
-Chapter chapter2 = new Chapter ("BDT",pages);
-chapters.Add (chapter1);
 
-mybook.Start
+mybook.Chapters = chapters;
+mybook.Start();
