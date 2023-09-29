3ca6a1cc8c0a9a30437d9492fe3228d4af7b4591
Sebastian <seba172i@mail.hansenberg.dk>
2023-09-29 09:46:29 +0200

    Attempting page rework
    and failing

--- MyNotes.cs
+++ MyNotes.cs
@@ -1,9 +1,34 @@
+using System;
+using System.IO;
+using System.Text;
 public static class MyNotes
 {
     public static string VariablesPageOne =
-@"...
-varibles 1
-";
+class Test
+{
+    public static void Main()
+    {
+        string path = @"c:SourceCodeProHansenberg\MeinNotes\Varibles1.txt";
+
+        // This text is added only once to the file.
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            string createText = "Hello and Welcome" + Environment.NewLine;
+            File.WriteAllText(path, createText);
+        }
+
+        // This text is always added, making the file longer over time
+        // if it is not deleted.
+        string appendText = "This is extra text" + Environment.NewLine;
+        File.AppendAllText(path, appendText);
+
+        // Open the file to read from.
+        string readText = File.ReadAllText(path);
+        Console.WriteLine(readText);
+    }
+}
+;
 
     public static string VariablesPageTwo =
 @"...
